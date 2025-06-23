3b7fd5f4a9423ac8b212815f531b9cc2d8e9457a
pradhokot <yoggi60@gmail.com>
2025-06-23 11:55:02 +0700

    2025 june 23, update 3

--- templates/template1/layout/footbar.js
+++ templates/template1/layout/footbar.js
@@ -2,9 +2,14 @@ document.writeln(`
 <footer class="footbar fs-small">
    <div class="container-fluid container-content py-4">
       <div class="row">
-         <div class="col col-12">
-            <p class="mb-0">
+         <div class="col col-12 col-lg">
+            <p class="mb-1">
                &copy; 1971 - <script>document.write(new Date().getFullYear())</script> INDRACO. All rights reserved
+            </p>
+         </div><!-- end col -->
+         <div class="col col-12 col-lg-auto">
+            <p class="mb-1">
+               Develop by &#x2665;&#xfe0f; INDRACODEV
             </p>
          </div><!-- end col -->
       </div><!-- end row -->
