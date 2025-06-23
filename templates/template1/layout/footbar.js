3d52a9d8d2bc24aee4e712fabc337aaf0b490d36
pradhokot <yoggi60@gmail.com>
2025-06-23 11:36:21 +0700

    2025 june 23, update 2

--- templates/template1/layout/footbar.js
+++ templates/template1/layout/footbar.js
@@ -1,9 +1,9 @@
 document.writeln(`
-<footer class="footbar small">
+<footer class="footbar fs-small">
    <div class="container-fluid container-content py-4">
       <div class="row">
          <div class="col col-12">
-            <p>
+            <p class="mb-0">
                &copy; 1971 - <script>document.write(new Date().getFullYear())</script> INDRACO. All rights reserved
             </p>
          </div><!-- end col -->
--- templates/template1/layout/sidebar.js
+++ templates/template1/layout/sidebar.js
@@ -1,13 +1,13 @@
 document.writeln(`
-<aside id="sidebar" class="offcanvas-lg offcanvas-start text-capitalize position-fixed top-0 start-0 bottom-0" z-index="-1">
-   <div class="offcanvas-header p-3 py-0 d-flex justify-content-lg-center px-lg-4">
+<aside id="sidebar" class="offcanvas-lg offcanvas-start text-capitalize position-fixed top-0 start-0 bottom-0 h-100 d-flex flex-column pb-4" z-index="-1">
+   <div class="offcanvas-header flex-grow-0 d-flex justify-content-lg-center">
       <h1 class="fs-reset m-0 lh-base">
          <img src="../../assets/img/logo-indraco.png" width="160" height="auto" class="img-default" alt="">
          <img src="../../assets/img/logo-indraco-invert.png" width="160" height="auto" class="img-invert" alt="">
       </h1>
       <button class="btn-close rounded-0 shadow-none d-lg-none" data-bs-toggle="offcanvas" data-bs-target="#sidebar"></button>
    </div>
-   <div class="offcanvas-body p-3 py-0 px-lg-4">
+   <div class="offcanvas-body flex-grow-1 overflow-y-auto">
       <div id="menu" class="w-100">
          <div class="d-lg-none">
             <script src="layout/menu-header.js"></script>
@@ -76,6 +76,63 @@ document.writeln(`
                   <span class="link-text link-hover">transactional setting</span>
                </a>
                <div class="collapse d-lg-block" id="collapseMenu2" data-bs-parent="#menu">
+                  <ul class="nav submenu-list flex-column">
+                     <li class="nav-item submenu-item">
+                        <a href="#" class="nav-link submenu-link px-0 text-reset media">
+                           <div class="media-header">
+                              <span class="link-icon">
+                                 <i class="fas fa-gear"></i>
+                              </span>
+                           </div>
+                           <div class="media-body">
+                              <span class="link-text link-hover">submenu link</span>
+                           </div>
+                        </a>
+                     </li>
+                     <li class="nav-item submenu-item">
+                        <a href="#" class="nav-link submenu-link px-0 text-reset media">
+                           <div class="media-header">
+                              <span class="link-icon">
+                                 <i class="fas fa-gear"></i>
+                              </span>
+                           </div>
+                           <div class="media-body">
+                              <span class="link-text link-hover">submenu link</span>
+                           </div>
+                        </a>
+                     </li>
+                     <li class="nav-item submenu-item">
+                        <a href="#" class="nav-link submenu-link px-0 text-reset media">
+                           <div class="media-header">
+                              <span class="link-icon">
+                                 <i class="fas fa-gear"></i>
+                              </span>
+                           </div>
+                           <div class="media-body">
+                              <span class="link-text link-hover">submenu link</span>
+                           </div>
+                        </a>
+                     </li>
+                     <li class="nav-item submenu-item">
+                        <a href="#" class="nav-link submenu-link px-0 text-reset media">
+                           <div class="media-header">
+                              <span class="link-icon">
+                                 <i class="fas fa-gear"></i>
+                              </span>
+                           </div>
+                           <div class="media-body">
+                              <span class="link-text link-hover">submenu link</span>
+                           </div>
+                        </a>
+                     </li>
+                  </ul>
+               </div>
+            </li><!-- end menu item -->
+            <li class="nav-item menu-item toggle-collapse">
+               <a data-bs-toggle="collapse" href="#collapseMenu3" class="nav-link menu-link px-0 text-reset collapsed">
+                  <span class="link-text link-hover">master setting</span>
+               </a>
+               <div class="collapse d-lg-block" id="collapseMenu3" data-bs-parent="#menu">
                   <ul class="nav submenu-list flex-column">
                      <li class="nav-item submenu-item">
                         <a href="#" class="nav-link submenu-link px-0 text-reset media">
